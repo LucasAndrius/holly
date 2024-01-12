3443759c8d8f8ac7b4db04d774470e05dfa1f26c
Lucas <lukasandrios@gmail.com>
2024-01-12 01:20:17 -0300

    sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,18 +18,20 @@ export default function RootLayout({
 }) {
   return (
     <html
-      lang="pt-BR"
+      lang="pt-br"
       className="container max-w-[1440px] mx-auto scroll-smooth"
     >
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-PGNJM94J')`,
-        }}
-      />
+          }}
+        />
+      </head>
       <body
         className={`${inter.className} bg-myYellow/5 text-myBlack dark:bg-myDarkGreen/5 dark:text-white`}
       >
